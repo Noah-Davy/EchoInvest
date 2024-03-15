8676484b1bd84984ec597f2ab3b26623eaf07189
noah <noahdavy18@gmail.com>
2024-03-15 14:15:38 +0000

    inital navbar test

--- echoInvestFinal/settings.py
+++ echoInvestFinal/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'django-insecure-yc(2&scq6k=f7i=fu$p!=qpr76igmkg%x*jyg%t4o)!+)+hp8d'
+SECRET_KEY = 'django-insecure-yc(2&scq6k=f7i=fu$p!=qpr76igmkg%x*jyg%t4o)!+)+hp8d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -119,7 +119,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
