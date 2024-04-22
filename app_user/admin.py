7548079c42b24172c482a520df215f7bf65b8005
noah <noahdavy18@gmail.com>
2024-04-22 20:13:08 +0100

    adminpanel

--- app_user/admin.py
+++ app_user/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+from .forms import UserRegistrationForm
 
-# Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    add_form = UserRegistrationForm
+    model = CustomUser
+    list_display = ['username', 'email', 'first_name', 'last_name', 'is_staff']
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
