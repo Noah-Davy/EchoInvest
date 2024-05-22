d93b6e3993478709113c0bdf8d8871d512d2cf13
Noah-Davy <noahdavy18@gmail.com>
2024-05-22 14:02:28 +0100

    backendFinalRetry1

--- app_advisor/models.py
+++ app_advisor/models.py
@@ -1,9 +1,10 @@
 # app_advisor/models.py
 from django.db import models
-from django.contrib.auth.models import User
+from django.conf import settings
+
 
 class Portfolio(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     risk_score = models.IntegerField()
     risk_tolerance = models.CharField(max_length=255)
     portfolio_data = models.JSONField()  # To store the portfolio allocation and performance as JSON
@@ -12,4 +13,7 @@ class Portfolio(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f"Portfolio for {self.user.username} with risk score {self.risk_score}"
+        # Check if user is an instance of the custom user model
+        if isinstance(self.user, settings.AUTH_USER_MODEL):
+            return f"Portfolio of {self.user.email}"
+        return "Portfolio of an unknown user"
